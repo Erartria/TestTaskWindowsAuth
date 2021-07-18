f5c11760aae8ecefada8083c8f97fa8aaf6b07e3
Erartria <ujkwf0at4vgb@mail.ru>
2021-07-18 15:47:13 +0300

    feat: index page rewrited

--- TestTaskWindowsAuth/Server/Startup.cs
+++ TestTaskWindowsAuth/Server/Startup.cs
@@ -51,7 +51,8 @@ namespace TestTaskWindowsAuth.Server
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        { 
+            app.UseStaticFiles();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -66,7 +67,7 @@ namespace TestTaskWindowsAuth.Server
 
             app.UseHttpsRedirection();
             app.UseBlazorFrameworkFiles();
-            app.UseStaticFiles();
+            
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
